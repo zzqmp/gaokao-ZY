c49baaa90118eba95012e548886813e0f00b8b43
zzq <zzq18143751520@163.com>
2026-06-23 20:34:32 +0800

    fix: 用@neondatabase/serverless对接Prisma Postgres，自动降级JSON

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pg'],
+  serverExternalPackages: ['@neondatabase/serverless'],
 };
 
 export default nextConfig;
